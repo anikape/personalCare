b15a00e5aae657bbf49f492fee31f79cf0fd684f
Rauane Lima <rauanelima7@gmail.com>
2023-04-17 20:50:15 -0300

    Performance: componentes organizados

--- Web-personalCare/src/App.jsx
+++ Web-personalCare/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Depoiments from './components/Home/Depoiments'
-import Diferencials from './components/Home/Diferencials'
-import Header from './components/Home/Header'
-import Informations from './components/Home/Informations'
-import Services from './components/Home/Services'
+import Depoiments from './components/Depoiments'
+import Diferencials from './components/Diferencials'
+import Header from './components/Header'
+import Informations from './components/Informations'
+import Services from './components/Services'
 
 function App() {
 
